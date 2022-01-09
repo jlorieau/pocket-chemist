35b8751fca7ccd34b0777aad3770bae7bac5e1e9
J Lorieau <justin@lorieau.com>
2022-01-09 11:07:22 -0600

    Add click and cli entrypoint script

---
+++ src/pocketchemist/cli/__init__.py
@@ -0,0 +1,3 @@
+from .cli import pocketchemist
+
+__all__ = ('pocketchemist',)
---
+++ src/pocketchemist/cli/cli.py
@@ -0,0 +1,7 @@
+import click
+
+
+@click.command()
+def pocketchemist():
+    """A pocket chemist to analyze spectra and molecules."""
+    pass
