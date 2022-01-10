6f0cfb7649d8d258da14f339ab86e44eba1a3dab
J Lorieau <justin@lorieau.com>
2022-01-10 09:14:49 -0600

    Implement loading of plugins

--- src/pocketchemist/cli/cli.py
+++ src/pocketchemist/cli/cli.py
@@ -1,7 +1,20 @@
+from importlib.metadata import entry_points  # python >=3.8
+import logging
+
 import click
 
 
-@click.command()
-def pocketchemist():
+@click.group()
+@click.option('--debug', is_flag=True, default=False,
+              help="Display debugging information")
+def pocketchemist(debug):
     """A pocket chemist to analyze spectra and molecules."""
-    pass
+    # Turn on debugging, if specified
+    if debug:
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug("Debuggin mode turned ON")
+
+# load plugins
+for entrypoint in entry_points()['pocketchemist']:
+    pocketchemist.add_command(entrypoint.load())
