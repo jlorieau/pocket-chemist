fb1707564c7bd3cc8a360c943dff1bd38fde7d9f
J Lorieau <justin@lorieau.com>
2022-01-20 08:54:44 -0600

    Remove unused variables

--- tests/processors/test_fft.py
+++ tests/processors/test_fft.py
@@ -12,7 +12,7 @@ def test_fftprocessor_get_fft_func(module_name):
     """Test the FFTProcessor get_fft_func method"""
     # This method will raise a ModuleNotFoundError if the module could not
     # be found
-    fft_func = FFTProcessor.get_fft_func(module_name=module_name)
+    FFTProcessor.get_fft_func(module_name=module_name)
 
 
 @pytest.mark.parametrize('module_name',
@@ -21,4 +21,4 @@ def test_fftprocessor_get_ifft_func(module_name):
     """Test the FFTProcessor get_ifft_func method"""
     # This method will raise a ModuleNotFoundError if the module could not
     # be found
-    ifft_func = FFTProcessor.get_ifft_func(module_name=module_name)
+    FFTProcessor.get_ifft_func(module_name=module_name)
