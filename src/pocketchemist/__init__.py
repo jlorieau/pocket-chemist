4afea97f8194d06623e2fa333042456fb7ffc9e2
J Lorieau <justin@lorieau.com>
2022-02-08 03:58:14 -0600

    Version update

--- src/pocketchemist/__init__.py
+++ src/pocketchemist/__init__.py
@@ -2,4 +2,4 @@ from . import cli, processors, config, utils
 
 __all__ = ('cli', 'processors', 'config', 'utils')
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- src/pocketchemist/processors/__init__.py
+++ src/pocketchemist/processors/__init__.py
@@ -1,5 +1,5 @@
 """Classes and functions to conduct one or more processes"""
 from .processor import Processor, GroupProcessor
-from .fft import FFTProcessor
+from .fft import FFTProcessor, FFTType
 
-__all__ = ('Processor', 'GroupProcessor', 'FFTProcessor')
+__all__ = ('Processor', 'GroupProcessor', 'FFTProcessor', 'FFTType')
