1936008edf321daed264ddb1d7ca0c2bd3b693a2
J Lorieau <justin@lorieau.com>
2024-04-11 20:35:05 -0500

    Black formatting

--- src/xamin/utils/classes.py
+++ src/xamin/utils/classes.py
@@ -5,14 +5,17 @@ Utilities for classes
 
 def all_subclasses(cls):
     """Retrieve all subclasses, sub-subclasses and so on for a class
+
     Parameters
     ----------
     cls : Type
         The class object to inspect for subclasses.
+
     Returns
     -------
     subclasses : list
         The list of all subclasses.
+        
     Examples
     --------
     >>> class A(object): pass
@@ -22,5 +25,6 @@ def all_subclasses(cls):
     [<class 'disseminate.utils.classes.B'>, \
 <class 'disseminate.utils.classes.C'>]
     """
-    return cls.__subclasses__() + [g for s in cls.__subclasses__()
-                                   for g in all_subclasses(s)]
+    return cls.__subclasses__() + [
+        g for s in cls.__subclasses__() for g in all_subclasses(s)
+    ]
