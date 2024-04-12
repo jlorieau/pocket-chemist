4807ab13afac7605dc2704964a0f3c455fd9d7de
J Lorieau <justin@lorieau.com>
2024-04-11 20:22:10 -0500

    Implement assign_unique_names and tests

--- src/xamin/project/project.py
+++ src/xamin/project/project.py
@@ -5,39 +5,48 @@ A project containing data entries
 import typing as t
 from weakref import ref
 from pathlib import Path
+from dataclasses import dataclass
+from collections import OrderedDict
 
 from loguru import logger
 
-from .entry import Entry
+from .entry import Entry, HintType
+
+__all__ = ("Project",)
 
 
 class ProjectException(Exception):
     """Exception raised while processing a project"""
 
 
-class Project:
+EntriesType = t.OrderedDict[str, Entry]
+
+
+@dataclass(init=False)
+class Project(Entry):
     """A project containin data entries"""
 
     #: All of the currently opened projects
     _opened_projects = []
 
-    #: The project name
-    name = "default"
-
     #: A list of data entries in this project
-    entries = []
+    entries: EntriesType
 
-    def __init__(
-        self,
-        name: t.Optional[str] = None,
-        entries: t.Optional[t.List[Entry]] = None,
-    ):
+    def __init__(self, *args, entries: t.Optional[EntriesType] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+
         # Add self as a weakref to the listing opened projects
         Project._opened_projects.append(ref(self))
 
         # Set the attributes
-        self.name = name if isinstance(name, str) else self.name
-        self.entries = entries if entries is not None else []
+        self.entries = OrderedDict()
+        if entries is not None:
+            self.entries.update(entries)
+
+    @classmethod
+    def is_type(cls, path: Path, hint: HintType = None) -> bool:
+        """Override parent class method"""
+        return False
 
     @classmethod
     def opened(cls) -> t.List["Project"]:
@@ -45,67 +54,37 @@ class Project:
         projects = [p() for p in cls._opened_projects]
         return [p for p in projects if p is not None]  # Remove invalid weakrefs
 
-    @staticmethod
-    def _find_unique_name(
-        name: str, existing_names: t.Iterable[str], suffix: str = " ({number:1})"
-    ) -> str:
-        """Given a list of existing names produce a name that is unique.
+    def assign_unique_names(self):
+        """Assign unique names to this project's entries."""
+        # Find all of the absolute paths of entries
+        parts = [
+            entry.path.parent.absolute().parts
+            for entry in self.entries.values()
+            if getattr(entry, "path", None) is not None
+        ]
+        parts_t = map(set, zip(*parts))  # transpose
 
-        Examples
-        --------
-        >>> Project._find_unique_name('test', ('one', 'two'))
-        'test'
-        >>> Project._find_unique_name('one', ('one', 'two'))
-        'one (2)'
-        >>> Project._find_unique_name('one', ('one', 'one (2)'))
-        'one (3)'
-        """
-        if name not in existing_names:
-            return name
+        # Find up to which part the paths are all common
+        common_parts = []
+        for s in parts_t:
+            if len(s) > 1:
+                break
+            common_parts.append(s.pop())
 
-        for number in range(999):
-            new_name = name.strip() + suffix.format(number=number)
-            if new_name not in existing_names:
-                return new_name
+        # Find the names from the common parts
+        common_path = Path(*common_parts) if len(common_parts) > 1 else None
+        current_entries = list(self.entries.values())
+        self.entries.clear()
 
-        raise AssertionError(f"Could not find a unique name for {name}")
+        for i, entry in enumerate(current_entries, 1):
 
-    def append_file_entries(self, *args) -> int:
-        """Append new files entries from the given arguments and return the number of files
-        added."""
-        # Convert to paths
-        paths = [Path(i) for i in args if type(i) in (str, Path)]
+            # Create a name from the common_path, if possible, or create a dummy
+            # unknown name
+            if not hasattr(entry, "path") or entry.path is None:
+                name = f"unknown ({i})"
+            elif common_path is not None:
+                name = entry.path.relative_to(common_path)
+            else:
+                name = entry.path
 
-        # Find new paths for existing files
-        existing_paths = {
-            e.path.absolute() for e in self.entries if isinstance(e, Entry)
-        }
-        existing_names = {e.name for e in self.entries}
-        file_entries = []
-
-        for p in paths:
-            # Skip non-file paths or paths that have already been added
-            if not p.is_file() or p.absolute() in existing_paths:
-                continue
-
-            # Otherwise create a new FileEntry
-            name = self._find_unique_name(p.name, existing_names=existing_names)
-            new_entry = Entry(name=name, path=p)
-
-            # Add the entry to the new file_entries listing
-            file_entries.append(new_entry)
-
-            # Add the entry's name to the existing entry names (to avoid dupes)
-            existing_names.add(name)
-
-        if file_entries:
-            logger.debug(
-                "Adding FileEntries: " + f"{', '.join([str(e) for e in file_entries])}"
-            )
-
-        # Append the file entries
-        self.entries += file_entries
-
-        # emit signal
-        self.layoutChanged.emit()
-        return len(file_entries)
+            self.entries[str(name)] = entry
---
+++ tests/project/test_project.py
@@ -0,0 +1,56 @@
+"""Tests for Project classes"""
+
+from pathlib import Path
+from collections import OrderedDict
+
+from xamin.project import Project, TextEntry
+
+
+def test_project_assign_unique_names():
+    """Test the Project.assign_unique_names method"""
+    # Create some test entries
+    entries = OrderedDict(
+        (
+            ("a", TextEntry(path=Path("/") / "usr" / "local" / "bin" / "file1")),
+            ("b", TextEntry(path=Path("/") / "usr" / "local" / "bin" / "file2")),
+            ("c", TextEntry(path=Path("/") / "usr" / "local" / "src" / "source.cpp")),
+            ("d", TextEntry(path=Path("/") / "etc" / "http.conf")),
+        )
+    )
+
+    project = Project(path=None, entries=entries)
+    assert len(project.entries) > 0
+
+    # 1. Assign and check names
+    project.assign_unique_names()
+    a = str(Path("/") / "usr" / "local" / "bin" / "file1")
+    b = str(Path("/") / "usr" / "local" / "bin" / "file2")
+    c = str(Path("/") / "usr" / "local" / "src" / "source.cpp")
+    d = str(Path("/") / "etc" / "http.conf")
+
+    assert list(project.entries.keys()) == [a, b, c, d]
+
+    # 2. Try popping an element and the common denominator changes
+    del project.entries[d]
+
+    project.assign_unique_names()
+    a = str(Path("bin") / "file1")
+    b = str(Path("bin") / "file2")
+    c = str(Path("src") / "source.cpp")
+    assert list(project.entries.keys()) == [a, b, c]
+
+    # 3. Try popping an element and the common denominator changes
+    del project.entries[c]
+
+    project.assign_unique_names()
+    a = str(Path("file1"))
+    b = str(Path("file2"))
+    assert list(project.entries.keys()) == [a, b]
+
+    # 4. Try removing a path from an entry
+    project.entries[b].path = None
+
+    project.assign_unique_names()
+    a = str(Path("file1"))
+    b = "unknown (2)"
+    assert list(project.entries.keys()) == [a, b]
