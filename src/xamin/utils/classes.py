dc7a18f7a7b4955f37714cf4362f9f333de0e38d
J Lorieau <justin@lorieau.com>
2024-04-23 16:32:24 -0500

    Implement a Singleton metaclass

--- src/xamin/utils/classes.py
+++ src/xamin/utils/classes.py
@@ -27,3 +27,25 @@ def all_subclasses(cls):
     return cls.__subclasses__() + [
         g for s in cls.__subclasses__() for g in all_subclasses(s)
     ]
+
+
+class Singleton(type):
+    """A metaclass for making singleton classes
+
+    Examples
+    --------
+    >>> class Single(metaclass=Singleton):
+    ...     pass
+    >>> singleton_a = Single()
+    >>> singleton_b = Single()
+    >>> assert id(singleton_a) == id(singleton_b)  # The same object
+    """
+
+    def __init__(cls, *args, **kwargs):
+        cls._singleton_instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._singleton_instance is None:
+            instance = super().__call__(*args, **kwargs)
+            cls._singleton_instance = instance
+        return cls._singleton_instance
