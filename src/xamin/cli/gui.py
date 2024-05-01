f27604a5d3afb314240565ce72afba79aaa9efc1
J Lorieau <justin@lorieau.com>
2024-05-01 11:01:21 -0500

    Move global exception logging to main.py

--- src/xamin/cli/gui.py
+++ src/xamin/cli/gui.py
@@ -16,25 +16,7 @@ def add_command(root_command):
 @click.argument("args", nargs=-1)
 def gui(args):
     """The graphical user interface (GUI) arguments"""
-    import sys
     from ..gui import MainApplication, MainWindow
-
-    # Disable showing backtrace to stdout
-    logger.remove()
-    logger.add(sys.stderr, backtrace=False)
-
-    def excepthook(exc_type, exc_value, exc_traceback):
-        """Catch and log exceptions, then continue"""
-        # Allow Ctrl+C to exit from the terminal
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return None
-
-        # Log the error and do nothing
-        logger.opt(exception=(exc_type, exc_value, exc_traceback)).error("Exception")
-
-    # Set the global exception catching hook to use the logger
-    sys.excepthook = excepthook
 
     # Create the root app
     app = MainApplication(list(args))
--- src/xamin/cli/main.py
+++ src/xamin/cli/main.py
@@ -32,10 +32,26 @@ def get_root_command():
 
         # Configure logger and set default level
         if debug:
-            logger.add(sys.stderr, level="DEBUG", enqueue=True)
+            logger.add(sys.stderr, level="DEBUG", enqueue=True, backtrace=False)
             logger.debug("Debug mode ON")
         else:
-            logger.add(sys.stderr, level="WARNING", enqueue=True)
+            logger.add(sys.stderr, level="WARNING", enqueue=True, backtrace=False)
+
+        # Catch exceptions and log them
+        def excepthook(exc_type, exc_value, exc_traceback):
+            """Catch and log exceptions, then continue"""
+            # Allow Ctrl+C to exit from the terminal
+            if issubclass(exc_type, KeyboardInterrupt):
+                sys.__excepthook__(exc_type, exc_value, exc_traceback)
+                return None
+
+            # Log the error and do nothing
+            logger.opt(exception=(exc_type, exc_value, exc_traceback)).error(
+                "Exception"
+            )
+
+        # Set the global exception catching hook to use the logger
+        sys.excepthook = excepthook
 
     # Add subcommands with plugins
     results = pm.hook.add_command(root_command=root_command)
