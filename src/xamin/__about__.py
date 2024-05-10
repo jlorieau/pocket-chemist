8c881cf561d6397fd8af8041a6aba30fe77ccf78
J Lorieau <justin@lorieau.com>
2024-05-10 13:31:55 -0500

    Move configuration to pyproject.toml

---
+++ src/xamin/__about__.py
@@ -0,0 +1,16 @@
+# Version number (from https://semver.org)
+# 
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+#
+#   MAJOR version when you make incompatible API changes
+#   MINOR version when you add functionality in a backward compatible manner
+#   PATCH version when you make backward compatible bug fixes
+#
+# Additionally,
+#
+#   PRE-RELEASE version MAY be denoted by appending a hyphen and a series of dot 
+#   separated identifiers. Examples: 1.0.0-alpha, 1.0.0-alpha.1
+#   BUILD metadata MAY be denoted by appending a plus sign and a series of dot 
+#   separated identifiers. Examples: 1.0.0-alpha+001, 1.0.0+20130313144700,
+
+__version__ = "0.2.0"
