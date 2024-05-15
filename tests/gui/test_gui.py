b849d5574d338da442d2591f1f8c34a99b44e12a
J Lorieau <justin@lorieau.com>
2024-05-15 06:25:21 -0500

    Rename test file

---
+++ tests/gui/test_gui.py
@@ -0,0 +1,30 @@
+"""
+Tests for GUI classes.
+"""
+
+import pytest
+
+from xamin.gui.main_window import MainWindow
+
+
+@pytest.mark.parametrize("cls", (MainWindow,))
+def test_widgets(cls, qtbot):
+    """Test GUI classes "widgets" property"""
+    obj = cls()
+    widgets = obj.widgets
+
+    assert len(widgets) > 0
+
+    # Reset the widgets shouldn't create a new item
+    new_widgets = obj.widgets
+
+    mismatched_widgets = set()
+    for name, widget in widgets.items():
+        new_widget = new_widgets[name]
+        if id(widget) != id(new_widget):
+            mismatched_widgets.add(name)
+
+    assert len(mismatched_widgets) == 0, (
+        f"The following widgets for '{cls.__name__}' were re-created: "
+        f"{mismatched_widgets}"
+    )
