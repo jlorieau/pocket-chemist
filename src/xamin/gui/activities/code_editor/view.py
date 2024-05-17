ac95f3d0b85e7f101bcfd0a3122a16025217d815
J Lorieau <justin@lorieau.com>
2024-05-17 08:22:16 -0500

    Add custom font to code editor view

--- src/xamin/gui/activities/code_editor/view.py
+++ src/xamin/gui/activities/code_editor/view.py
@@ -2,7 +2,11 @@
 The Code Editor view
 """
 
+import typing as t
+
+from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QPlainTextEdit
+from thatway import Setting
 
 from .model import CodeEditorModel
 
@@ -18,6 +22,17 @@ class CodeEditorView(QPlainTextEdit):
         The document (CodeEditorModel) containing the text data to view
     """
 
+    #: The default font to use for the code editor
+    font_families: t.ClassVar[Setting] = Setting(
+        ("Monaco", "Courier", "Courier New"),
+        desc="Default font families for Code Editor",
+    )
+    font_size: t.ClassVar[Setting] = Setting(14, "Default font size for Code Editor")
+
     def __init__(self, document: CodeEditorModel) -> None:
         super().__init__()
         self.setDocument(document)
+
+        # Configure the widget
+        font = QFont(self.font_families, self.font_size)
+        self.setFont(font)
